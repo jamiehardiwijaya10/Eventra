0d6999c6a77b4f411e683e9bbef7f693cd99f34e
jamiehardiwijaya10 <jamiehardiwijaya@student.upi.edu>
2026-07-27 20:32:26 +0700

    Add routes to homescreen

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -5,6 +5,8 @@ import '../features/splash/screen/splash_screen.dart';
 import '../features/onboarding/screen/onboarding1.dart';
 import '../features/onboarding/screen/onboarding2.dart';
 import '../features/onboarding/screen/onboarding3.dart';
+import '../features/home/screen/home_screen_costumer.dart';
+import '../features/home/screen/home_screen_booth.dart';
 
 class AppRoutes {
   AppRoutes._();
@@ -14,6 +16,8 @@ class AppRoutes {
   static const onboarding1 = "/onboarding1";
   static const onboarding2 = "/onboarding2";
   static const onboarding3 = "/onboarding3";
+  static const homecostumer = "/home_costumer";
+  static const homebooth = "/home_booth";
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -45,6 +49,16 @@ class AppRoutes {
       case onboarding3:
         return MaterialPageRoute(
           builder: (_) => OnboardingScreen3(),
+        );
+
+      case homecostumer:
+        return MaterialPageRoute(
+          builder: (_) => HomeScreenCostumer(),
+        );
+
+      case homebooth:
+        return MaterialPageRoute(
+          builder: (_) => HomeScreenBooth(),
         );
 
       default:
